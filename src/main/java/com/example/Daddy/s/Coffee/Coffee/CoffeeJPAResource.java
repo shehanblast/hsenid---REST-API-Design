0250347d27b602ce3843ebc3d5d0fac0ea9575ca
shehanblast <shihanbinuka@gmail.com>
2020-12-25 10:30:56 +0530

    Add Get Mapping

--- src/main/java/com/example/Daddy/s/Coffee/Coffee/CoffeeJPAResource.java
+++ src/main/java/com/example/Daddy/s/Coffee/Coffee/CoffeeJPAResource.java
@@ -1,4 +1,24 @@
 package com.example.Daddy.s.Coffee.Coffee;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
 public class CoffeeJPAResource {
+
+    @Autowired
+    private CoffeeJPARepo coffeeJPARepo;
+
+    @GetMapping("/jpa/coffee/viewall")
+    public List<Coffee> getAllTodos(){
+
+        return coffeeJPARepo.findAll();
+
+    }
+
 }
